4408ee1c99f0da70e3941c892e43c7a8813c094b
helios <ilpuccio.febo@gmail.com>
2010-06-30 10:08:42 -0700

    +feature Plugin looks for plugins in .bioruby user directory and then in ssytemwide. TODO: check if the plugin is already loaded.

--- lib/bio/plugin/init.rb
+++ lib/bio/plugin/init.rb
@@ -1,11 +1,11 @@
 
 #Load all the plugins located in defined directories:
 #lib/bio/shell/plugin
+require 'rubygems'
 
+#load plugin: first into home directory and then into the bioruby distribution.
 
-#load plugin 
-
-Dir.glob(File.join(File.dirname(__FILE__),'../shell/plugin/*')) do |plugin|
+Dir.glob([File.join(Gem.user_home,".bioruby/shell/plugin/*"), File.join(File.dirname(__FILE__),'../shell/plugin/*')]) do |plugin|
   if File.directory?(plugin)
     $: << File.join(plugin,'lib')
     require File.join(plugin,'init.rb')
